3f3797ffe9fc15a821559cc1e8e75d98dccbc1ff
sandykr29 <sandeepkumar29774@yahoo.com>
2024-04-25 00:20:14 +0530

    done

--- Triangle.js
+++ Triangle.js
@@ -1,65 +1,89 @@
-// //eg:1 Square
+// Note:- In the given problem , for shapeType Triangle making rows and columns different is not providing desired look so rows must be equal to columns
 
-// function Squre(n){
-//     for(let i=0;i<n;i++){
-//       let str = "";
-//       for(let j=0;j<n;j++){
-//         str += "* ";
-//       }
-//       console.log(str);
-//     }
-//   }
-//   console.log(Squre(10));
+function drawShape(shapeType, alignment, drawChar, rows, columns) {
+    if (shapeType === "square" && alignment === "solid") {
+        Squre(drawChar, rows, columns);
+    } else if (shapeType === "square" && alignment === "hollow") {
+        HollowSqure(drawChar, rows, columns);
+    } else if (shapeType === "triangle" && alignment === "left") {
+        TriangleLeft(rows, drawChar);
+    } else if (shapeType === "triangle" && alignment === "right") {
+        TriangleRight(rows, drawChar);
+    } else if (shapeType === "triangle" && alignment === "center") {
+        TriangleCenter(rows, drawChar);
+    } else {
+        console.log("Invalid shape type or alignment.");
+    }
+  }
   
-// //   Hollow squre
-//   function Squre(n, m) {
-//     for (let i = 0; i < n; i++) {
-//       let str = "";
-//       for (let j = 0; j < m; j++) {
-//         if (i === 0 || i === n - 1 || j == 0 || j == m - 1) {
-//           str += "*";
-//         } else str += " ";
-//       }
-//       console.log(str);
-//     }
-//   }
-//   console.log(Squre(15, 15));
+  // Given functions
   
-
-// //eg:2 Left Triangle
-
-
-// function LeftTriangle(n){
-//     let str = ""
-//     for(let i=1;i<=n;i++){
-//       for(let k=1;k<=i;k++){
-//         str += "A";
-//       }
-//       if(i%2){
-        
-//       console.log(str);
-//       }
-//       str = "";
-//     }
-//   }
+  function TriangleRight(rows, drawChar) {
+    for (let i = 0; i <= rows; i++) {
+        let row = '';
+        for (let j = rows; j >= 0; j--) {
+            if (j < i) {
+                row += `${drawChar}`;
+            } else {
+                row += ' ';
+            }
+        }
+        console.log(row);
+    }
+  }
   
-//   console.log(LeftTriangle(5));
+  function TriangleLeft(rows, drawChar) {
+    for (let i = 0; i < rows; i++) {
+        let str = "";
+        for (let k = 0; k <= i; k++) {
+            str += `${drawChar}`;
+        }
+        console.log(str);
+    }
+  }
   
-
-
-// // Eg:3
-
-// function TriangleWithOne(n){
-//     for(let i=0;i<n;i++){
-//       let str = "";
-//       for(let j=0;j<n-i;j++){
-//         str += " ";
-//       }
-//       for(let k=0;k<=i;k++){
-//         str += "8 ";
-//       }
-//       console.log(str);
-//     }
-//   }
+  function TriangleCenter(rows, drawChar) {
+    for (let i = 0; i < rows; i++) {
+        let str = "";
+        for (let j = 0; j < rows - i; j++) {
+            str += " ";
+        }
+        for (let k = 0; k <= i; k++) {
+            str += `${drawChar} `;
+        }
+        console.log(str);
+    }
+  }
   
-//   console.log(TriangleWithOne(10))
+  function Squre(drawChar, rows, columns) {
+    for (let i = 0; i < rows; i++) {
+        let str = "";
+        for (let j = 0; j < columns; j++) {
+            str += `${drawChar} `;
+        }
+        console.log(str);
+    }
+  }
+  
+  function HollowSqure(drawChar, rows, columns) {
+    for (let i = 0; i < rows; i++) {
+        let str = "";
+        for (let j = 0; j < columns; j++) {
+            if (i === 0 || i === rows - 1 || j === 0 || j === columns - 1) {
+                str += `${drawChar}`;
+            } else {
+                str += " ";
+            }
+        }
+        console.log(str);
+    }
+  }
+  
+  // Example usage keep uncommenting one by one...
+  
+  // drawShape(shapeType, alignment, drawChar, rows, columns)
+  // drawShape("square", "solid", "*", 5, 5);
+  // drawShape("square", "hollow", "*", 5, 5);
+  // drawShape("triangle", "right", "*", 5, 5);
+  // drawShape("triangle", "left", "*", 5, 5);
+  // drawShape("triangle", "center", "*", 5, 5);
